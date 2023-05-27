8004f0933a8fba7f2ec549dc41ebcc5232adc226
Minalshelke02 <114931983+Minalshelke02@users.noreply.github.com>
2023-05-27 15:15:17 +0530

    initial draft

---
+++ src/main/java/PraticeExample/AreaOfRectangle.java
@@ -0,0 +1,15 @@
+package PraticeExample;
+
+public class AreaOfRectangle {
+    public static void main(String args[]){
+        int height=7;
+        int width=5;
+
+       int  area=height*width;
+       int perimeter= 2* (height + width);
+
+        System.out.println("area of retangle: " +area);
+        System.out.println("perimeter of retangle: " +perimeter);
+
+    }
+}
