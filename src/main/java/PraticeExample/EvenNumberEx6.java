e7cb58a2b640f7ef1ea723f0b03d7c4915826a30
Minalshelke02 <114931983+Minalshelke02@users.noreply.github.com>
2023-05-30 23:31:27 +0530

    initial draft

---
+++ src/main/java/PraticeExample/EvenNumberEx6.java
@@ -0,0 +1,15 @@
+package PraticeExample;
+
+public class EvenNumberEx6 {
+    public static void main(String args[]){
+
+       for(int i=1; i<=50;i++){
+           if(i%2==0){
+
+
+            System.out.println(i);
+
+           }
+       }
+    }
+}
