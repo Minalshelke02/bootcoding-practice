0bfa1c8442f5c88f1a65019714bebb81c297f776
Minalshelke02 <114931983+Minalshelke02@users.noreply.github.com>
2023-05-31 22:39:25 +0530

    initial draft

---
+++ src/main/java/PraticeExample/Ex1.java
@@ -0,0 +1,10 @@
+package PraticeExample;
+
+public class Ex1 {
+    public static void main(String args[]){
+
+        System.out.println("Name :Alexandra Abramov ");
+        System.out.println("DOB : July 14, 1975");
+        System.out.println("Mobile:99-9999999999");
+    }
+}
