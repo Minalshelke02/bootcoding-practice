b262eb4525b079349526297661c5a0efcff30531
Minalshelke02 <114931983+Minalshelke02@users.noreply.github.com>
2023-06-02 22:40:37 +0530

    initial draft

---
+++ src/main/java/Practice2/even.java
@@ -0,0 +1,19 @@
+package Practice2;
+
+public class even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	 int no= 13;
+
+	 if(no % 2==0) {
+		 System.out.println(no + " No is Even");
+	 }
+	
+	else {
+		System.out.println(no +" is odd");
+	}
+
+}
+}
