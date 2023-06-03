1bff08ec137016d2e6ce789d019a7b86070bc32f
Minalshelke02 <114931983+Minalshelke02@users.noreply.github.com>
2023-06-03 22:39:50 +0530

    initial draft

---
+++ src/main/java/Practice2/Factorial.java
@@ -0,0 +1,16 @@
+package Practice2;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+	
+		int no=4;
+		int fact=1;
+		for(int i=1; i<=no; i++) {
+			
+			fact= fact*1;
+		}
+System.out.println("factorial of " + no + " is " + fact) ;
+	}
+
+}
